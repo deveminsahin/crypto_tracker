88d6782e88be976cbf8b72a79027900fd34bb2d4
eminsahinradity <seeminsahin@gmail.com>
2026-02-07 20:34:28 +0300

    refactor(main): update app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,13 @@
+import 'package:crypto_tracker/app/app.dart';
+import 'package:crypto_tracker/core/di/service_locator.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 
-import 'package:crypto_tracker/core/theme/app_theme.dart';
-import 'package:crypto_tracker/providers/market_provider.dart';
-import 'package:crypto_tracker/repositories/market_repository.dart';
-import 'package:crypto_tracker/router/app_router.dart';
-import 'package:crypto_tracker/services/api_service.dart';
-import 'package:crypto_tracker/services/isolate_parser.dart';
-import 'package:crypto_tracker/services/websocket_config.dart';
-import 'package:crypto_tracker/services/websocket_service.dart';
-
-void main() {
+/// Application entry point.
+///
+/// Initialises the Flutter binding, sets up the service locator
+/// (which opens the ObjectBox store), then launches the root widget.
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupServiceLocator();
   runApp(const CryptoTrackerApp());
-}
-
-class CryptoTrackerApp extends StatelessWidget {
-  const CryptoTrackerApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) {
-        final apiService = ApiService(http.Client());
-        final wsService = WebSocketService(WebSocketConfig.production);
-        final isolateParser = IsolateParser();
-
-        final repository = MarketRepository(
-          apiService: apiService,
-          webSocketService: wsService,
-          isolateParser: isolateParser,
-        );
-
-        return MarketProvider(repository: repository);
-      },
-      child: MaterialApp.router(
-        title: 'Crypto Tracker',
-        theme: AppTheme.darkTheme,
-        routerConfig: appRouter,
-        debugShowCheckedModeBanner: false,
-      ),
-    );
-  }
 }
